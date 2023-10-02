6af56967bd56db2d229563b8f4cdea7c86fbb9a4
Mauricio <117490557+mauricio-oliveira-dos-santos@users.noreply.github.com>
2023-10-01 21:36:03 -0300

    Add files via upload

---
+++ desafio.js
@@ -0,0 +1,35 @@
+class herois {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque = "";
+        switch (this.tipo) {
+            case "mago":
+                ataque = "usou magia";
+                break;
+
+            case "guerreiro":
+                ataque = "usou espada";
+                break;
+
+            case "monge":
+                ataque = "usou artes marciais";
+                break;
+
+            case "ninja":
+                ataque = "usou shuriken";
+                break;
+
+            default:
+                ataque = "usou um ataque indefinido";
+        }
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+let heroi = new herois("Warrior", 38, "ninja");
+heroi.atacar();
